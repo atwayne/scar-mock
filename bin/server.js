6582f2cc15454c39c1a5ef9d16c594fac8394ff0
Wayne Wang <nkgailei@gmail.com>
2021-03-19 20:22:36 +0800

    lint fix

--- bin/server.js
+++ bin/server.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
-const http = require("http");
+const http = require('http');
 
 const getPort = () => {
-    const defaultPort = 9000;
-    const argv = process.argv.slice(2);
-    if (argv.length === 2 && argv[0] === "--port") {
-        return argv[1];
-    }
-    return defaultPort;
+  const defaultPort = 9000;
+  const argv = process.argv.slice(2);
+  if (argv.length === 2 && argv[0] === '--port') {
+    return argv[1];
+  }
+  return defaultPort;
 };
 
 const port = getPort();
-const server = require("../lib/server");
+const server = require('../lib/server');
 
 http.createServer(server.requestListener).listen(port, () => {
-    console.log(`Server listening on port ${port}...`);
+  console.log(`Server listening on port ${port}...`);
 });
